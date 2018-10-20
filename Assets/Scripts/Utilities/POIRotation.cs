d79236995ff385203f13615aece330e29f43d20c
Tien Nguyen Khac <crazycat194hb@gmail.com>
2018-10-20 15:42:21 +1300

    Apply speed multiplier to POI rotation animator

---
+++ Assets/Scripts/Utilities/POIRotation.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using GalaxyExplorer;
+
+public class POIRotation : MonoBehaviour {
+
+    Animator POIRotationAnim;
+
+	void Start () {
+        POIRotationAnim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+        Debug.Log(POIRotationAnim.GetCurrentAnimatorStateInfo(0).fullPathHash);
+        if (POIRotationAnim.GetCurrentAnimatorStateInfo(0).IsName("GalaxyRotation"))
+        {
+            POIRotationAnim.speed = GalaticController.instance.speedMultiplier;
+        }
+	}
+}
