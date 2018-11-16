cb04afb6e7d340d68b79c15db78c3f58ff1fc579
Tien Nguyen Khac <crazycat194hb@gmail.com>
2018-11-16 15:34:38 +1300

    Add BackendService script

---
+++ Assets/Scripts/Utilities/BackendService.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+
+namespace GalaxyExplorer
+{
+    public class BackendService : MonoBehaviour
+    {
+
+        public static BackendService instance = null;
+
+        void Awake()
+        {
+            if (instance == null)
+            {
+                instance = this;
+            }
+
+
+            else if (instance != this)
+            {
+                Destroy(gameObject);
+            }
+
+            DontDestroyOnLoad(gameObject);
+        }
+
+        public void LogAction(string actionTarget)
+        {
+            WWWForm formData = new WWWForm();
+            formData.AddField("target", actionTarget);
+
+            UnityWebRequest rq = UnityWebRequest.Post("http://localhost:3000/api/action-log", formData);
+            rq.SendWebRequest();
+        }
+
+    }
+}
