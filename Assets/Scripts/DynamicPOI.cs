e6f47a8399169644e153d7e393c016198f4017bd
Matt Crook <mcro591@aucklanduni.ac.nz>
2018-12-11 12:25:46 +1300

    move parent transform to a local var

--- Assets/Scripts/DynamicPOI.cs
+++ Assets/Scripts/DynamicPOI.cs
@@ -7,12 +7,11 @@ namespace GalaxyExplorer
 {
     public class DynamicPOI : MonoBehaviour
     {
-        private Transform parent_POIRot;
         public void PlotItems(string context)
         {
             if (context == "GalaxyView")
             {
-                parent_POIRot = transform.Find(context + "Content/SceneLoadHider/HeroView/POIRotation");
+                Transform parent = transform.Find(context + "Content/SceneLoadHider/HeroView/POIRotation");
 
                 // get the elements to plot
                 ChronozoomLoader loader = new ChronozoomLoader();
@@ -29,13 +28,13 @@ namespace GalaxyExplorer
         }
 
         // plot a point of interest programmatically
-        void PlotPOI(string name, string cardText, string transitionScene, Vector3 position)
+        void PlotPOI(string name, string cardText, string transitionScene, Vector3 position, Transform parent)
         {
             // setup structure
             GameObject poi = (GameObject)Instantiate(
                 Resources.Load("Prefabs/POI", typeof(GameObject)),
                 position, Quaternion.identity,
-                parent_POIRot);
+                parent);
 
             poi.name = name;
             poi.transform.Find("ScaleWithDistance target/POI").gameObject.GetComponent<PointOfInterest>().TransitionScene = transitionScene;
