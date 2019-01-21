923ebfe226670f2e03fd9d0944cf900bb2ca14c3
Matt Crook <mcro591@aucklanduni.ac.nz>
2019-01-21 14:11:31 +1300

    bezier component for curved paths

---
+++ Assets/Scripts/Components/Bezier.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+
+namespace GalaxyExplorer
+{
+    public static class Bezier {
+        public static Vector3 GetPoint (Vector3 p0, Vector3 p1, Vector3 p2, float t)
+        {
+            return Vector3.Lerp(Vector3.Lerp(p0, p1, t), Vector3.Lerp(p1, p2, t), t);
+        }
+    }
+}
---
+++ Assets/Scripts/Components/BezierCurve.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GalaxyExplorer
+{
+    public class BezierCurve : MonoBehaviour {
+        public Vector3[] points;
+
+        public Vector3 GetPoint (float t)
+        {
+            return transform.TransformPoint(Bezier.GetPoint(points[0], points[1], points[2], t));
+        }
+    }
+}
