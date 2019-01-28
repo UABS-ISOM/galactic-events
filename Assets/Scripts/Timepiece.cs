eeba26c4d3e637a3af7885f5d02329e59668fa74
Matt Crook <mcro591@aucklanduni.ac.nz>
2019-01-28 13:31:00 +1300

    update the timepiece as years go by

---
+++ Assets/Scripts/Timepiece.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Globalization;
+using UnityEngine;
+
+namespace GalaxyExplorer
+{
+    public class Timepiece : MonoBehaviour
+    {
+        private TextMesh tooltip;
+        void Start()
+        {
+            tooltip = transform.Find("Tooltip").GetComponent<TextMesh>();
+            GameObject.Find("/ViewLoader").GetComponent<Timekeeper>()
+                .updateActions.Add(year =>
+                {
+                    tooltip.text = string.Format("Year: {0}",
+                        year.ToString("N0", CultureInfo.InvariantCulture));
+                });
+        }
+    }
+}
